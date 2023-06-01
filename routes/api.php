4f78e42b0706f8973a0bf03e79baab997387a767
Max Oliveira <max-oliveira@outlook.com.br>
2023-06-01 15:26:28 -0300

    feat: create user

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BarberController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/logout', [AuthController::class, 'logout']);
 Route::post('/auth/refresh', [AuthController::class, 'refresh'] );
-Route::post('users', [AuthController::class, 'create ']);
+
+Route::post('/users', [AuthController::class, 'create']);
+
+
 
 Route::get('/users', [UserController::class, 'read']);
+Route::put('/users', [UserController::class, 'read']);
 Route::get('/users/favorites', [UserController::class, 'getFavorites']);
 Route::post('/users/favorites', [UserController::class, 'addFavorites']);
 Route::get('/users/appointments', [UserController::class, 'getAppointments']);
@@ -30,6 +35,6 @@ Route::post('/users/appointments', [UserController::class, 'addAppointments']);
 
 Route::get('/barbers', [BarberController::class, 'list']);
 Route::get('/barber/{id}', [BarberController::class, 'one']);
-Route::post('/barber/{id}/appointment', [BarberController::class, 'one']);
+Route::post('/barber/{id}/appointment', [BarberController::class, 'setAppointment']);
 
 Route::get('search', [BarberController::class, 'search']);
