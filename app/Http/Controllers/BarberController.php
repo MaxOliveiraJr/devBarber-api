cd4dc946ec97f1eef8cb52dd1aefaf1e20f99405
Max Oliveira <max-oliveira@outlook.com.br>
2023-06-01 17:50:50 -0300

    random create

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BarberController extends Controller
 {
+    Private $loggerUser;
+    public function __construct(){
 
+        $this->middleware('auth:api');
+        $this->loggerUser = auth()->user();
+    }
+
+    public function createRandom(){
+
+    }
 }
