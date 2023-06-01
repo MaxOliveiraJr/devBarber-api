c4e7da9de37cef82a4e8290e09dc26396fddfcf2
Max Oliveira <max-oliveira@outlook.com.br>
2023-06-01 14:25:52 -0300

    feat: routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh'] );
+Route::post('users', [AuthController::class, 'create ']);
 
-Route::get('/ping', function (){
-    return ['pong'=>true];
-});
+Route::get('/users', [UserController::class, 'read']);
+Route::get('/users/favorites', [UserController::class, 'getFavorites']);
+Route::post('/users/favorites', [UserController::class, 'addFavorites']);
+Route::get('/users/appointments', [UserController::class, 'getAppointments']);
+Route::post('/users/appointments', [UserController::class, 'addAppointments']);
+
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barber/{id}', [BarberController::class, 'one']);
+Route::post('/barber/{id}/appointment', [BarberController::class, 'one']);
+
+Route::get('search', [BarberController::class, 'search']);
